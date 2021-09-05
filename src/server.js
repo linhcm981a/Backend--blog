571fb8a8ba99e22a27f9423ddc59cb392f27c01e
linhcm981a <linhcm981a@gmail.com>
2021-09-05 19:32:53 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+import connectDB from "./config/connectDB";
+
+
+
+require("dotenv").config();
+
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ express: true }))
+
+
+viewEngine(app);
+initWebRoutes(app);
+
+connectDB();
+
+
+let port = process.env.PORT ;
+app.listen(port, () => {
+    console.log("Backend-Nodejs is runing on port : " +port)
+})
